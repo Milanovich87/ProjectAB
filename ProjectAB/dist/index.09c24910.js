ba268fe88ee4aaf844c43213632ecb4104b87364
Milanovich87 <milanovich198701@gmail.com>
2022-07-20 23:21:01 +0300

    projecttwo

--- ProjectAB/dist/index.09c24910.js
+++ ProjectAB/dist/index.09c24910.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(".header__burger").click(function(event) {
-        $(".header__burger,.menu").toggleClass("active");
+        $(".header__burger,.header__menu").toggleClass("active");
         $("body").toggleClass("lock");
     });
 });
